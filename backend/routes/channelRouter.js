a73da0746dd8c92d494cf75061aada771ebe4cc7
Mohammed <mohdfaheem.1205@gmail.com>
2024-06-30 20:12:36 +0530

    channel creation complete

--- backend/routes/channelRouter.js
+++ backend/routes/channelRouter.js
@@ -6,25 +6,8 @@ const jwt = require('jsonwebtoken');
 
 const secretKey = 'secretkey';
 
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
 
-    if (!token) {
-        return res.sendStatus(401); // Unauthorized if token is not provided
-    }
-
-    jwt.verify(token, secretKey, (err, user) => {
-        if (err) {
-            return res.sendStatus(403); // Forbidden if token is invalid
-        }
-        console.log(user);
-        req.user = user; // Attach user information to request object for further handling
-        next(); // Move to next middleware or route handler
-    });
-};
-
-router.post('/channels',authenticateToken, async(req,res) => {
+router.post('/channels', async(req,res) => {
     try{
         const { name, description } = req.body;
         const channel = await Channel.create({name,description});
--- chatapp/src/components/NewChannelButton.jsx
+++ chatapp/src/components/NewChannelButton.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function NewChannelButton() {
     const [showPopup, setShowPopup] = useState(false);
@@ -33,6 +34,24 @@ function NewChannelButton() {
         setDescription('');
     }
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            const response = await axios.post('/api/channels',{
+            name: name,
+            description: description,
+            });
+            if (!response.status){
+                console.log('Error with sending post request');
+            }
+            console.log('submitted');
+            handleClosePopup();
+        } catch(error){
+            console.log('error occured',error);
+        }
+
+    }
+
     return (
         <>
             <button 
@@ -45,11 +64,11 @@ function NewChannelButton() {
             {showPopup && (
                 <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
                     <div className="absolute inset-0 bg-gray-800 opacity-75"></div>
-                    <div className="relative bg-white px-4 pt-6 pb-2 rounded-lg shadow-lg w-2/4">
-                        <form className="flex flex-col">
+                    <div className="relative bg-white px-4 py-3 rounded-lg shadow-lg w-2/4">
+                        <form className="flex flex-col" id="create" onSubmit={handleSubmit}>
                             <label htmlFor="name">Channel Name:</label>
                             <input 
-                                className="border mb-4 p-2"
+                                className="border mb-4 p-2 h-7"
                                 type="text"
                                 id="name"
                                 value={name}
@@ -57,7 +76,7 @@ function NewChannelButton() {
                             />
                             <label htmlFor="description">Channel Description:</label>
                             <input 
-                                className="border mb-4 p-2"
+                                className="border mb-4 p-2 h-7"
                                 type="text"
                                 id="description"
                                 value={description}
@@ -66,9 +85,11 @@ function NewChannelButton() {
                             <div className="flex justify-end">
                             </div>
                         </form>
-                        <div className="flex justify-end">
+                        <div className="flex justify-between items-center">
+                            <button form="create" type="submit" className="bg-zinc-200 rounded-sm px-5 py-1 hover:bg-gray-400 transition duration-75">Create</button>
                             <p className="text-gray-400">Press Esc to Close</p>
                         </div>
+                       
                     </div>
                 </div>
             )}
